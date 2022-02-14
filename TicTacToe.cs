5af913149b61047d6327d1cf52d27935995ec028
Sukalyan8 <96485897+Sukalyan8@users.noreply.github.com>
2022-02-14 18:58:58 +0530

    Add UC-1 Program

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -8,5 +8,22 @@ namespace TicTacToeGame
 {
     class TicTacToe
     {
+        public static void TikTakToe_Game()
+        {
+            char[] startarr = new char[10];
+            for (int i = 0;i<startarr.Length;i++)
+            {
+                if(i==0)
+                {
+                    continue;
+                }
+                startarr[i] = ' ';
+
+            }
+            Console.WriteLine("\t|\t|*");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("\t|\t|*");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("\t|\t|*");
     }
 }
