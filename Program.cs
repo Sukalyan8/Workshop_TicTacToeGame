73e36906d01539d097261dadd463b64838f16d7b
Sukalyan8 <96485897+Sukalyan8@users.noreply.github.com>
2022-02-15 11:49:45 +0530

    UC2 File Added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to Tic-Tac-Toe!");
+            TicTacToe game = new TicTacToe();
+            //game.CreateBoard();
+            game.Choice();
+        }
+    }
+}
---
+++ TicTacToe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+
+        public char[] board;
+        public enum Player { USER, COMPUTER };
+
+
+        // Initializes a new instance 
+
+        public TicTacToe()
+        {
+            board = new char[10];
+        }
+
+        // UC1
+        // Creates the board.
+
+        public void CreateBoard()
+        {
+            for (int position = 1; position < 10; position++)
+            {
+                board[position] = ' ';
+            }
+        }
+        /// UC2
+        // Choices this instance.
+
+        public char Choice()
+        {
+            Console.WriteLine("Enter your choice. \n X \n 0");
+            char userSign = Convert.ToChar(Console.ReadLine());
+            string choice;
+            switch (userSign)
+            {
+                case 'X':
+                    choice = "You Chose: X";
+                    break;
+                case '0':
+                    choice = "You Chose: 0";
+                    break;
+                default:
+                    choice = "Invalid Choice";
+                    break;
+            }
+            Console.WriteLine(choice);
+            if (choice == "Invalid Choice")
+                Choice();
+            return userSign;
+
+        }
+    }
+}
